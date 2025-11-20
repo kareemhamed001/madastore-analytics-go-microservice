ac2ce35b107d3e6907cda9b43724f5e099d8033b
kareemhamed001 <khidmt55@gmail.com>
2025-11-20 16:33:28 +0200

    feat: add GetVisitsPerCityForToday method to DashboardAnalysisRepositoryInterface

--- internal/repository/interfaces/analytics_repositories_interface.go
+++ internal/repository/interfaces/analytics_repositories_interface.go
@@ -11,6 +11,7 @@ type DashboardAnalysisRepositoryInterface interface {
 	GetVisitsPerMonth(ctx context.Context) ([]models.VisitsPerMonthData, error)
 	GetVisitsPerCountry(ctx context.Context) ([]models.VisitsPerCountryData, error)
 	GetVisitsPerCity(ctx context.Context) ([]models.VisitsPerCityData, error)
+	GetVisitsPerCityForToday(ctx context.Context) ([]models.VisitsPerCityData, error)
 	GetVisitsFromEgyptPerDay(ctx context.Context) ([]models.VisitsFromEgyptData, error)
 	GetVisitsFromOtherCountriesPerDay(ctx context.Context) ([]models.VisitsFromCountriesData, error)
 	GetVisitsFromEgyptPerHourForPastMonth(ctx context.Context) ([]models.VisitsFromEgyptHoursData, error)
